22241be5c534318e29a199601d366a53413dd24a
Gianux77 <gfg4899@gmail.com>
2023-10-18 22:51:48 -0300

    neti-1.0

--- js/myscript.js
+++ js/myscript.js
@@ -44,9 +44,8 @@
         linkTerr.style.display  =   "block";
         imageList.innerHTML = ""; // Limpiar la lista de imágenes
         // Agregar las imágenes del territorio que desees mostrar aquí
-        showImage("img/territorio/octubre1.jpeg", "Territorio 1");
+        showImage("img/territorio/octubre2.jpeg", "Territorio 1");
         showImage("img/territorio/map.jpeg", "Territorio 2");
-        showImage("img/territorio/octubre_21.jpeg", "Territorio 3");
         // Agregar más imágenes si es necesario
     });
 
